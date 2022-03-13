e256e636c854c2efb8e0c9d7767797d242aee407
Ryan Collins <ryan@ohmgeek.co.uk>
2022-03-13 10:16:39 +0000

    Build the kernel from scratch (no need to use Fedora's one anymore)

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Build options
+LINUX_BUILD_TYPE="defconfig"
+LINUX_BUILD_PARALLEISM=4
+LINUX_ARCH="x86"
+
 function print_options() {
     echo "--------------------------"
     echo "VM-OS Build Script"
@@ -13,6 +18,27 @@ function build_init() {
     cd init/
     ./build.sh
     cd ../
+}
+
+function build_linux() {
+    if [ -f linux/arch/${LINUX_ARCH}/boot/bzImage ]; then
+        echo "Skipping linux build, as bzImage already exists."
+    else
+        echo "Building linux..."
+        cd linux/
+        make clean
+        # Build the config files, needed to build the actual kernel
+        make allnoconfig
+        # Build kvm config specifically
+        make ${LINUX_BUILD_TYPE}
+        # Build the code, TODO set parallelism
+        make -j${LINUX_BUILD_PARALLEISM}
+        cd ../
+        echo "Linux built successfully"
+    fi
+
+    # As a helper, copy bzImage to top level.
+    cp linux/arch/${LINUX_ARCH}/boot/bzImage ./bzImage
 }
 
 function build_image() {
@@ -31,4 +57,5 @@ print_options
 ## Start by cleaning
 rm -rf bootdisk/
 build_init
+build_linux
 build_image
